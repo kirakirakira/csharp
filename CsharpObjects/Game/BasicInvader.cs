e37ebd8a77095965ae1661a27d1f0691c93ae5f6
Kira Hartlage <kira.hartlage@gmail.com>
2018-09-12 19:31:44 -0400

    added BasicInvader class

---
+++ CsharpObjects/Game/BasicInvader.cs
@@ -0,0 +1,10 @@
+namespace TreehouseDefense
+{
+    class BasicInvader : Invader
+    {
+        public BasicInvader(Path path) : base(path)
+        {
+
+        }
+    }
+}
--- CsharpObjects/Game/Invader.cs
+++ CsharpObjects/Game/Invader.cs
@@ -1,12 +1,12 @@
 namespace TreehouseDefense
 {
-    class Invader
+    abstract class Invader
     {
         private readonly Path _path;
         private int _pathStep = 0;
         protected virtual int StepSize { get; } = 1;
         public MapLocation Location => _path.GetLocationAt(_pathStep);
-        public virtual int Health { get; protected set; } = 2;
+        public abstract int Health { get; protected set; }
 
         // True if the invader has reached the end of the path
         public bool HasScored

52eef7aaebc898cfc8ba928a7b3362afcd11224a
Kira Hartlage <kira.hartlage@gmail.com>
2018-10-12 16:46:41 -0400

    started unit testing course

---
+++ TreehouseDefense/TreehouseDefense.Tests/NUnitTestClass.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using TreehouseDefense;
+
+namespace TreehouseDefense.Tests
+{
+    [TestFixture()]
+    public class PointTests
+    {
+        [Test()]
+        public void PointTest()
+        {
+            int x = 5;
+            int y = 6;
+
+            var point = new Point(x, y);
+            Assert.Equals(x, point.X);
+            Assert.Equals(y, point.Y);
+        }
+
+        [Test()]
+        public void DistanceToTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
